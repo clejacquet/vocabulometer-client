a922433059c5fcdbb9d341964b7eb30cd3ec23f4
Clement Jacquet <clem.jacquet@gmail.com>
2018-04-10 19:43:43 +0900

    update env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://vocabulometer-dev.herokuapp.com',
+  serverUrl: 'http://10.127.11.133',
   gazeUrl: 'http://localhost:8080'
 };
